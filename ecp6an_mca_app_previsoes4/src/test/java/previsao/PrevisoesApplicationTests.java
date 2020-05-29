7f670518af986e7200ebd65dba1e2b54b55e1989
Regiane <Regiane@DESKTOP-BJEA6QT>
2020-05-29 17:16:23 -0300

    Aula11

---
+++ ecp6an_mca_app_previsoes4/src/test/java/previsao/PrevisoesApplicationTests.java
@@ -0,0 +1,13 @@
+package previsao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrevisoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
